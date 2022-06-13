9b352187a1a3063063a2b6f65d83a69fd2136b8c
omarfaruk5510 <107416142+omarfaruk5510@users.noreply.github.com>
2022-06-13 20:20:13 +0600

    Add files via upload

---
+++ c_basic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int main()
+{
+    printf("Name   : Alexander Abramov\n");
+    printf("DOB    : July 14,1975\n");
+    printf("Mobile : 99-9999999999\n");
+}
+return 0;
+}
